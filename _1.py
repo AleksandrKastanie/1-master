c2707209cc8dcb1908c604ca5b0cb4ea71a41c8d
opilane <opilane@HP-CZC6297BB7>
2021-11-16 16:42:39 +0200

    Update

--- _1.py
+++ _1.py
@@ -1,11 +1,61 @@
-#28 задание по лотарее ---------------------------
+#Практическая работа по математике------------------------------
 from random import *
-print("Loterii".center(50,"*"))
-computer=randint(1,100)
-chel=int(input("введите число "))
-while chel!=computer: 
-    chel=input("вы проиграли "+ chel+ " попробуйте снова")
-print("Вы отгадали")
+from math import *
+from keyboard import * 
+print("Добро пожаловать на тест по матиматике, Выберите сложность от 1 до 3".center(60,"*"))
+Hard=0
+while Hard not in [1,2,3]:
+    try: 
+        Hard=int(input("Сложность: "))
+    except ValueError:
+        print("Только 1, 2, 3!")
+    else:
+        print("Просят же, 1, 2 или 3!")
+if Hard==1:
+   min=2
+   max=10
+   tehed=["+","-"]
+elif Hard==2:
+   min=2
+   max=20
+   tehed=["+","-","*"]
+else:
+    min=-20
+    max=30
+    tehed=["+","-","*","//"]
+p=0
+kokku=0
+while True:
+    v=input("Продолжаем? esc - выйти, space - продолжаем")
+    if v=="stop":
+        break
+    else:
+        kokku+=1
+        a=randint(min, max)
+        b=randint(min, max)
+        Hard=choice(tehed)# работает только с модулем Random и выбирает из списка значение 
+        print(f"{a}{Hard}{b}=", end=" ")
+        õige=eval(str(a)+Hard+str(b))
+        vastus=int(input("="))
+        if vastus==int(õige):
+             print("Правильный ответ")
+             p+=1
+        else:
+             print("Не правильный ответ")
+
+
+
+
+
+
+#28 задание по лотарее ---------------------------
+#from random import *
+#print("Loterii".center(50,"*"))
+#computer=randint(1,10)
+#chel=int(input("введите число "))
+#while chel=computer: 
+#    chel=input("вы проиграли "+ chel + " попробуйте снова")
+#print("Вы отгадали")
 
 
 ##задание с while 
@@ -13,7 +63,7 @@ print("Вы отгадали")
 #while loom.title()!="Слон": #upper(), lower() 
 #    loom=input("Все говорят: "+ loom+"! А ты купи!!!")
 #print("С вас 1.000.000 евро")
-##17
+##17----------------------------------------------
 #for rjady in range(1,10):
 #    for stroka in range(1,10):
 #       if rjady==stroka:
@@ -24,7 +74,7 @@ print("Вы отгадали")
 #           print("0", end=" ")
 #    print()
 
-##16
+##16-------------------------------------------------
 #for rjady in range(1,10):
 #    for stroka in range(1,10):
 #       if rjady==stroka:
